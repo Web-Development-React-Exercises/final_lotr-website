6f59a48c517fa0986ad24ba0f41d8ef586017b83
PortalFan202 <anibaldonoso1@gmail.com>
2022-11-04 00:43:32 +0200

    Created Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../logo.png';
 
 export default function Header() {
     return (
-        <header>
-            <h1>Header</h1>
+        <header id="header">
+            <div className="logo"><img src={logo} alt="Page logo" /></div>
+            <ul class="headerLinkContainer">
+                <li class="headerLink"><Link to="/">Home</Link></li>
+                <li class="headerLink"><Link to="/books">Books</Link></li>
+                <li class="headerLink"><Link to="/movies">Movies</Link></li>
+                <li class="headerLink"><Link to="/characters">Characters</Link></li>
+                <li class="headerLink"><Link to="/quotes">Quotes</Link></li>
+                <li class="headerLink"><Link to="/chapters">Book Chapters</Link></li>          
+                <li class="headerLink"><Link to="/about">About</Link></li>
+            </ul>
         </header>
     );
 }
