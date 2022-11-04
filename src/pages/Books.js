1f4aa511e5a5c844410b6cd36102b0df8a65b2ab
PortalFan202 <anibaldonoso1@gmail.com>
2022-11-04 23:30:00 +0200

    Added catch error

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -4,26 +4,33 @@ import Header from "../components/Header";
 import { useEffect, useState } from 'react';
 
 import getBooks from '../functions/getBooks';
+import getBookChapters from '../functions/getBookChapters';
 
 export default function Books() {
-    
     const [books, setBooks] = useState([]);
 
     useEffect(() => {
+        //On page load, get all books from API
         getBooks().then((data) => {
-            setBooks(data.docs);
-            console.log(books);
+            const response = data.docs
+            setBooks(response);
+        }).catch((error) => {
+            console.log(error);
         });
+     
     }, []);
 
     return (
         <div>
             <Header />
-            {books.map(books=>
-                <div>
-                    <h1>{books.name}</h1>
+            {books.map(book => {
+                var x =JSON.parse(JSON.stringify(book));
+                console.log(x);
+
+                return <div>
+                    <h1>{book.name}</h1>
                 </div>
-            )}
+            })}
         </div>
     );
 }
