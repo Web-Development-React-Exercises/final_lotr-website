1dc9bdf62ca3fae22b676750339a8f55ea56fbd8
PortalFan202 <anibaldonoso1@gmail.com>
2022-11-05 19:14:11 +0200

    Increased the limit of gotten quotes of getQuotes

---
+++ src/functions/getQuotes.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export default async function getQuotes() {
+    try {
+        //make axios request to URL
+        const response = await axios.get(process.env.REACT_APP_URL + "/quote", {
+            headers: {
+                Authorization: process.env.REACT_APP_API_KEY
+            }
+        });
+        //return response data
+        return response.data;
+
+
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: err.toString(),
+        };
+    }
+}
