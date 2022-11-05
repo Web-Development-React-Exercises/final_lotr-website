0b52a96d8bff72b7e8cc88f73ae3c40f980cec4b
PortalFan202 <anibaldonoso1@gmail.com>
2022-11-05 23:07:51 +0200

    Advanced in about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,15 @@ export default function About() {
     return (
         <div>
             <Header />
-            <h1>About</h1>
+            <div className="main center">
+                <h1 className="title">About</h1>
+                <div className="about">
+                    <p>This is a fairly simple page I made to test The One API and also as my final assignment for my Web Development course.</p>
+                    <p>The repository can be found at</p>
+                    <a href="">Github</a>
+                    <p>Any inquiries about the page please send them to anibaldonoso1@gmail.com</p>
+                </div>
+            </div>
         </div>
     );
 }
