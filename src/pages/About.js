7985e93adb3daec616bdfb13646f6c2890c3c222
PortalFan202 <anibaldonoso1@gmail.com>
2022-11-05 23:13:30 +0200

    Added more into to About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,6 +13,8 @@ export default function About() {
                     <p>The repository can be found at</p>
                     <a href="https://github.com/Web-Development-React-Exercises/final_lotr-website">Github</a>
                     <p>Any inquiries about the page please send them to anibaldonoso1@gmail.com</p>
+                    <p>Special Thanks to The One API for providing its service free of charge, the API can be found here:</p>
+                    <a href="https://the-one-api.dev">The One API</a>
                 </div>
             </div>
         </div>
