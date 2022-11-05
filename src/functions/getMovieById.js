5a56da2f66db326ed306638cd643f1996bec0b27
PortalFan202 <anibaldonoso1@gmail.com>
2022-11-05 19:40:31 +0200

    Fixed error in both functions

--- src/functions/getMovieById.js
+++ src/functions/getMovieById.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async function getMovieById(id) {
     try {
-        const result = await axios.get(process.env.REACT_APP_URL + "/movie/" + book_id, {
+        const result = await axios.get(process.env.REACT_APP_URL + "/movie/" + id, {
             headers: {
                 Authorization: process.env.REACT_APP_API_KEY
             }
