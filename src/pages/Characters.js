1c3a21f314360ba69b7a9207234df9fb241424fa
PortalFan202 <anibaldonoso1@gmail.com>
2022-11-05 23:01:33 +0200

    Created Characters page

--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -1,12 +1,44 @@
 import React from "react";
 
 import Header from "../components/Header";
+import { useEffect, useState } from "react";
+
+import getCharacters from "../functions/getCharacters";
 
 export default function Characters() {
+    const [characters, setCharacters] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    useEffect(() => {
+        getCharacters().then((data) => {
+            setCharacters(data.docs);
+        })
+    }, [])
+
     return (
         <div>
             <Header />
-            <h1>Characters</h1>
+            <div className="main center">
+                <h1 className="title">Characters</h1>
+                <input className="input" type="text" placeholder="Search" onChange={(e) => setSearchTerm(e.target.value)} />
+                <div className="characters">
+                    {characters.filter((char)=>{
+                        if(searchTerm == ""){
+                            return char;
+                        }else if(char.name.toLowerCase().includes(searchTerm.toLowerCase())){
+                            return char;
+                        }
+                    }).map((char) => {
+                        return (
+                            <div className="characterContainer">
+                                <h2>{char.name}</h2>
+                                <p>{char.race}</p>
+                                <p>{char.gender}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
         </div>
     );
 }
