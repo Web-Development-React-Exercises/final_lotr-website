0206a8dc2c13e19188dadfdacc29412427ec688f
PortalFan202 <anibaldonoso1@gmail.com>
2022-11-05 19:31:13 +0200

    Basic functionalities to the quotes page

--- src/functions/getQuotes.js
+++ src/functions/getQuotes.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default async function getQuotes() {
     try {
         //make axios request to URL
-        const response = await axios.get(process.env.REACT_APP_URL + "/quote", {
+        const response = await axios.get(process.env.REACT_APP_URL + "/quote/?limit=2400", {
             headers: {
                 Authorization: process.env.REACT_APP_API_KEY
             }
--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -1,12 +1,40 @@
 import React from "react";
 
 import Header from "../components/Header";
+import { useEffect, useState } from "react";
+
+import getQuotes from "../functions/getQuotes";
 
 export default function Quotes() {
+    const [quotes, setQuotes] = useState([]);
+    const [displayedQuote, setDisplayedQuote] = useState("Click me to get a random quote");
+    const [loadingQuotes, setLoadingQuotes] = useState(true);
+
+    useEffect(() => {
+        getQuotes().then((data) => {
+            setQuotes(data.docs);
+            setLoadingQuotes(false);
+        })
+    }, [])
+
+    function setRandomQuote() {
+        setDisplayedQuote(quotes[Math.floor(Math.random() * quotes.length)].dialog);
+    }
+
     return (
         <div>
             <Header />
-            <h1>Quotes</h1>
+            <div className="main center">
+                <h1 className="title">Random Quote</h1>
+                {loadingQuotes ? <h1>Loading...</h1> :
+                    <div className="quote center">
+                        <div className="quoteContainer" onClick={() => setRandomQuote()}>
+                            <h1>{displayedQuote}</h1>
+                        </div>
+                        <button className="copy" onClick={() => {navigator.clipboard.writeText(displayedQuote)}}>Copy text</button>
+                    </div>
+                }
+            </div>
         </div>
     );
 }
